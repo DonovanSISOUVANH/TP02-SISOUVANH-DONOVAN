ed3588174aeb08714cdb84db1dff9694234ecd71
Donovan <donogf@hotmail.fr>
2021-03-03 14:42:43 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormulaireComponent } from './formulaire/formulaire.component';
+import { FooterComponent } from './footer/footer.component';
+import { TetiereComponent } from './tetiere/tetiere.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormulaireComponent,
+    FooterComponent,
+    TetiereComponent
   ],
   imports: [
     BrowserModule
